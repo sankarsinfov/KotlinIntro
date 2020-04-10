ec658525c49b6dbcef036f95fbe85df34890c384
SANKAR B <bcsankar@gmail.com>
2020-04-10 17:09:19 +0530

    Data type changed

--- app/src/main/java/com/example/sample1/MainActivity.kt
+++ app/src/main/java/com/example/sample1/MainActivity.kt
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    var name:String?=null
+    var a:Integer?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
