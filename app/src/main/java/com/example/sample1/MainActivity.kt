ccaf6f2198b5890f37bd779d6f9df631dab1170d
SANKAR B <bcsankar@gmail.com>
2020-04-12 15:17:08 +0530

    Edit text and button used

--- app/src/main/java/com/example/sample1/MainActivity.kt
+++ app/src/main/java/com/example/sample1/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.sample1
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
-    var a:Integer?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var firstValue=findViewById(R.id.editText1)as EditText
+        var secondValue=findViewById(R.id.editText2)as EditText
+        var result=findViewById(R.id.editText3)as EditText
+        var btnAdd=findViewById(R.id.button)as Button
+
+        btnAdd.setOnClickListener {
+
+
+            result.setText(firstValue.text.toString().toInt()+secondValue.text.toString().toInt())
+        }
     }
 }
+
